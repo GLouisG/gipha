59709136a1be3a1c5e8fb34893b223feaefbfb3e
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-26 11:46:40 +0300

    add GetTrendings Function to return link for the gif trending apis

---
+++ src/app/data-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';//import from angular
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  constructor(private http:HttpClient, ) {//inject allows for comm with the api
+  }
+  getTrendings(){
+    const {apiKey} = environment;
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?
+    api_key=${apiKey}&limit=25&rating=g`);
+  }
+}
